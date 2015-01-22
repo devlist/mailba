b8934896f0fe2c5836938aa22e5e0e070c51cad0
Witoo Harianto <witoo@outlook.com>
2015-01-22 10:13:11 +0700

    update

--- mailba.go
+++ mailba.go
@@ -1,5 +1,7 @@
 package mailba
 
+//go:generate mockgen -destination=mock.go --self_package=github.com/plimble/mailba -package=mailba github.com/plimble/mailba Sender
+
 type Config map[string]interface{}
 
 type Sender interface {
