1f4d2160db336d82835767382988b857811fd12d
Witoo Harianto <witoo@outlook.com>
2015-03-16 01:25:24 +0700

    change mock

---
+++ mock_mailba/sender.go
@@ -0,0 +1,20 @@
+package mock_mailba
+
+import "github.com/plimble/mailba"
+import "github.com/stretchr/testify/mock"
+
+type Sender struct {
+	mock.Mock
+}
+
+func NewSender() *Sender {
+	return &Sender{}
+}
+
+func (m *Sender) Send(mail *mailba.Mail, config *mailba.Config) error {
+	ret := m.Called(mail, config)
+
+	r0 := ret.Error(0)
+
+	return r0
+}
